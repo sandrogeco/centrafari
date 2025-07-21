d2b752e56f4bbc4cefa12e1fccfaa47146eebfe0
sandrogeco <sandro@geco-company.com>
2025-07-21 10:40:29 +0200

    inizio script autoexp

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import logging
+import numpy as np
 
 
 def apri_camera():
@@ -14,14 +15,20 @@ def apri_camera():
 
 def set_camera(i, config):
     logging.info(f"set_camera /dev/video{i}")
-
     try:
         #os.system(f"v4l2-ctl --device /dev/video{i} --set-ctrl=exposure_auto={config['exposure_auto']}")
         os.system(f"v4l2-ctl --device /dev/video{i} --set-ctrl=brightness={config['brightness']}")
         os.system(f"v4l2-ctl --device /dev/video{i} --set-ctrl=contrast={config['contrast']}")
         os.system(f"v4l2-ctl --device /dev/video{i} --set-ctrl=saturation={config['saturation']}")
         os.system(f"v4l2-ctl --device /dev/video{i} --set-ctrl=exposure_absolute={config['exposure_absolute']}")
-
         os.system(f"v4l2-ctl --device /dev/video{i} --list-ctrls")
+    except Exception as e:
+        logging.error(f"error: {e}")
+
+def autoexp(i,image_input):
+    logging.info(f"autoexp /dev/video{i}")
+    try:
+        r=np.max(image_input)
+        os.system(f"v4l2-ctl --device /dev/video{i} --set-ctrl=exposure_auto={config['exposure_auto']}")
     except Exception as e:
         logging.error(f"error: {e}")
--- prove_gamma.py
+++ prove_gamma.py
@@ -2,19 +2,56 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 # Dati
-gamma_impostato = np.array([100, 200, 300, 400, 500])
-valori = np.array([42, 55, 72, 92, 109])  # Valori pixel letti
-valori = np.array([90, 108, 127, 142, 155])  # Valori pixel letti
 
-# Modello
-def risposta_gamma(gamma_fittizio, a, b, I):
-    gamma_reale = a * gamma_fittizio + b
-    return 255 * (I ** (1 / gamma_reale))
 
-# Fit
-popt, _ = curve_fit(risposta_gamma, gamma_impostato, valori, bounds=([0, 0, 0], [0.01, 10, 1]))
-a, b, I = popt
 
-# Risultato
-print(f"a = {a:.5f}, b = {b:.5f}, I = {I:.5f}")
-print(f"gamma reale = a * gamma_impostato + b")
+
+
+exp = np.array([250,300,350,400,450,500,550,600,650,700,750,800])
+valori = np.array([60,94,118,144,166,183,199,212,219,225,231,236])  # Valori pixel letti
+
+exp = np.array([50,60,70,80,90,100,110,125])
+valori = np.array([101,136,166,190,210,218,225,237])
+
+exp = np.array([150,200,250,300,350,400,450,500,550])
+valori = np.array([44,96,135,166,194,214,223,233,237])
+
+
+
+def risposta(exp,g, l):
+    #c = 12**g/255**(g-1)
+    c=0
+    a=255-c
+    r=255*((a*(1-np.exp(-l*exp))+c)/255)**(1/g)
+    return r
+
+
+
+# Fit con limiti ragionevoli
+popt, _ = curve_fit(
+    risposta,
+    exp,
+    valori,
+  #  p0=[2,30],
+    bounds=([ 0, 0], [ np.Inf, 1e-1])  # gamma 0.5–5, l tra 0.0001 e 1
+)
+
+g, l = popt
+print(f" gamma = {g:.6e}, lambda = {l:.6f}")
+
+
+import matplotlib.pyplot as plt
+
+# Genera punti per il fit
+exp_fit = np.linspace(min(exp), max(exp), 200)
+valori_fit = risposta(exp_fit, *popt)
+
+# Plot
+plt.plot(exp, valori, 'o', label='Dati misurati')
+plt.plot(exp_fit, valori_fit, '-', label='Modello fit')
+plt.xlabel('Tempo di esposizione')
+plt.ylabel('Valore pixel')
+plt.title('Fit risposta sensore con saturazione e gamma')
+plt.grid(True)
+plt.legend()
+plt.show()
